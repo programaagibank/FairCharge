bc18b9bed1ad5c3322964f4a57928916ea8002ac
Fava <gr.favareto@gmail.com>
2025-03-12 17:00:20 -0300

    Add - conexão com o banco de dados

--- src/main/java/br/com/agi/Main.java
+++ src/main/java/br/com/agi/Main.java
@@ -1,4 +1,15 @@
 package br.com.agi;
 
+import java.sql.Connection;
+
 public class Main {
+    public static void main(String[] args) {
+        try (Connection connection = databaseConnection.getConnection()) {
+            if (connection != null) {
+                System.out.println("Conexão bem-sucedida!");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/br/com/agi/databaseConnection.java
@@ -0,0 +1,28 @@
+package br.com.agi;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class databaseConnection {
+    private static final String PROPERTIES_FILE = "src/main/resources/database.properties";
+
+    public static Connection getConnection() {
+        Properties properties = new Properties();
+
+        try (FileInputStream fis = new FileInputStream(PROPERTIES_FILE)) {
+            properties.load(fis);
+            String url = properties.getProperty("db.url");
+            String user = properties.getProperty("db.username");
+            String password = properties.getProperty("db.password");
+
+            return DriverManager.getConnection(url, user, password);
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao conectar ao banco de dados", e);
+        }
+    }
+}
