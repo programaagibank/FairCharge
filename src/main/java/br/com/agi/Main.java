e487768cf8be9fc0ecdf720d1846584063d39e23
Lilian <liliansod22@gmail.com>
2025-03-13 23:10:07 -0300

    Primeira versão RelatorioFaturamentoView

---
+++ src/main/java/br/com/agi/Main.java
@@ -0,0 +1,23 @@
+package br.com.agi;
+
+import br.com.agi.view.MenuView;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Connection connection = databaseConnection.getConnection()) {
+            if (connection != null) {
+                System.out.println("Conexão bem-sucedida!");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
+        }
+
+        //todo, Integração do Menu de seleção inicial *Igor*
+        MenuView menu = new MenuView();
+        menu.ExibirMenuInicial();
+
+
+    }
+}
