1b8121394d2752896bb2b1a241cd74019166e4fc
joao.felicio <felicio.joaopaulodev@gmail.com>
2025-03-14 08:23:52 -0300

    Adição da tela de login

---
+++ src/main/java/br/com/agi/view/LoginView.java
@@ -0,0 +1,8 @@
+package br.com.agi.view;
+
+public class LoginView {
+    public void telaBoasVindas() {
+    System.out.println("============== Bem Vindo ao FairCharge =============\nSeu sistema de Gerenciamento de Pagamento e Cobrança");
+
+    }
+}
