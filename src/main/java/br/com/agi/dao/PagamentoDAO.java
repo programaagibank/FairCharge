09fd6205f9c86338875aefa6414e370aa5cd4baf
gabriel.favareto <gr.favareto@gmail.com>
2025-03-26 09:28:16 -0300

    Ajustes - ExibirMenu ordem do switch

--- src/main/java/br/com/agi/dao/PagamentoDAO.java
+++ src/main/java/br/com/agi/dao/PagamentoDAO.java
@@ -1,7 +1,6 @@
 package br.com.agi.dao;
 
 import br.com.agi.database.databaseConnection;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- src/main/java/br/com/agi/view/inicios/ExibirMenu.java
+++ src/main/java/br/com/agi/view/inicios/ExibirMenu.java
@@ -18,11 +18,11 @@ public class ExibirMenu {
         while (true) {
             System.out.print("\n================================\n");
             System.out.println("1 - Menu Relatorio de Faturamento");
-            System.out.println("2 - Ajustar taxas");
-            System.out.println("3 - Listar cobrancas por cliente");
-            System.out.println("4 - Gerenciar Usuário");
-            System.out.println("5 - Sair");
-            System.out.println("6 - Listar Pagamentos"); // Nova opção adicionada
+            System.out.println("2 - Listar Pagamentos");
+            System.out.println("3 - Ajustar taxas");
+            System.out.println("4 - Listar cobrancas por cliente");
+            System.out.println("5 - Gerenciar Usuário");
+            System.out.println("6 - Sair");
 
             System.out.print(":");
             opcao = sc.next().charAt(0);
@@ -35,23 +35,23 @@ public class ExibirMenu {
                     menu.TelaMenu();
                     break;
                 case '2':
-                    TaxaMenuView taxaMenuView = new TaxaMenuView();
-                    taxaMenuView.telaInicial();
-                    break;
-                case '3':
-                    System.out.println("🚧 Lista de Cobrancas - Em desenvolvimento... ");
-                    break;
-                case '4':
-                    gerenciadorUsuario.GerenciadorDeUsuario();
-                    break;
-                case '5':
-                    System.out.println("Saindo do sistema... Até logo!");
-                    return;
-                case '6':
-                    System.out.println("Listar Pagamentos");
+                    System.out.println("Listando Pagamentos, aguarde...");
                     PagamentoDAO listaPagamentos = new PagamentoDAO();
                     listaPagamentos.listarPagamentos();
                     break;
+                case '3':
+                    TaxaMenuView taxaMenuView = new TaxaMenuView();
+                    taxaMenuView.telaInicial();
+                    break;
+                case '4':
+                    System.out.println("🚧 Lista de Cobrancas - Em desenvolvimento... ");
+                    break;
+                case '5':
+                    gerenciadorUsuario.GerenciadorDeUsuario();
+                    break;
+                case '6':
+                    System.out.println("Saindo do sistema... Até logo!");
+                    return;
                 default:
                     System.out.println("Opção inválida! Tente novamente.");
             }
