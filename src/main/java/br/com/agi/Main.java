3f443a5559b40231a5be14547ad8edd392e9a117
gabriel.favareto <gr.favareto@gmail.com>
2025-03-28 10:05:27 -0300

    Ajuste de permissoes na tabela

--- src/main/java/br/com/agi/Main.java
+++ src/main/java/br/com/agi/Main.java
@@ -1,31 +1,31 @@
-package br.com.agi;
-import br.com.agi.database.databaseConnection;
-import br.com.agi.view.inicios.TelaInicial;
-import br.com.agi.view.inicios.WelcomeView;
+//package br.com.agi;
+//import br.com.agi.database.databaseConnection;
+//import br.com.agi.view.inicios.TelaInicial;
+//import br.com.agi.view.inicios.WelcomeView;
+//
+//
+//import java.sql.Connection;
+//
+//public class Main {
+//    private static WelcomeView welcome = new WelcomeView();
+//    private static TelaInicial telaInicial = new TelaInicial();
+//
+//    public static void main(String[] args) {
+//        try (Connection connection = databaseConnection.getConnection()) {
+//            if (connection != null) {
+//                System.out.println("Conexao bem-sucedida!");
+//            }
+//        } catch (Exception e) {
+//            System.out.println("🚧telaBoasVindas(); Erro ao conectar: " + e.getMessage());
+//            return;
+//        }
+//
+//        welcome.telaBoasVindas();
+//        telaInicial.TelaMenu();
+//    }
+//}
 
 
-import java.sql.Connection;
-
-public class Main {
-    private static WelcomeView welcome = new WelcomeView();
-    private static TelaInicial telaInicial = new TelaInicial();
-
-    public static void main(String[] args) {
-        try (Connection connection = databaseConnection.getConnection()) {
-            if (connection != null) {
-                System.out.println("Conexao bem-sucedida!");
-            }
-        } catch (Exception e) {
-            System.out.println("🚧telaBoasVindas(); Erro ao conectar: " + e.getMessage());
-            return;
-        }
-
-        welcome.telaBoasVindas();
-        telaInicial.TelaMenu();
-    }
-}
-
-/*
 package br.com.agi;
 
 import br.com.agi.database.databaseConnection;
@@ -53,8 +53,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Navegador.setPrimaryStage(primaryStage); // Define o Stage principal
-        Navegador.getLogin(); // Exibe a tela inicial
+        Navegador.setPrimaryStage(primaryStage);
+        Navegador.getLogin();
     }
 }
-*/
