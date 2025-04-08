476f463857887b16bef0e27438238e533be4e6c0
tulio.neto <tulio.jacinto32@gmail.com>
2025-04-08 08:30:41 -0300

    fix home

--- src/main/java/br/com/agi/controller/fx/HomeController.java
+++ src/main/java/br/com/agi/controller/fx/HomeController.java
@@ -7,17 +7,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class HomeController {
-    @FXML private Button gerenciarUsuarioButton;
 
     @FXML
     void handleAjustarTaxas() {
         Navegador.getMenuTaxa();
     }
 
-    @FXML
-    void handleGerenciarUsuario() {
-        Navegador.getGerenciadorUsuario();
-    }
 
     @FXML
     void handleListarCobrancasCliente() {
@@ -37,18 +32,6 @@ public class HomeController {
     @FXML
     void handleSair() {
         Navegador.getLogin();
-    }
-
-
-
-    @FXML
-    public void initialize() {
-        Usuario usuarioLogado = SessaoLogon.getLoggedUser();
-
-        if (usuarioLogado != null && !usuarioLogado.isAdmin()) {
-            gerenciarUsuarioButton.setVisible(false);
-            gerenciarUsuarioButton.setManaged(false);
-        }
     }
 
 }
